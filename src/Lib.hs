5ffa9b754ec78f63429f8ddde4b8929d08e23c6f
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2015-12-25 17:49:54 +0200

    Update endpoints and some queries

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,33 @@
 module Lib
-    ( someFunc
+    ( queryCD
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Time
+
+endpoint = "http://api.coindesk.com/v1/bpi/"
+
+getCurrentPrice = endpoint ++ "currentprice.json"
+
+-- | 
+-- ++ <code>.json, EUR.json
+getCurrentPriceInCurrency :: String -- ^ Currency code for conversion
+                          -> String 
+getCurrentPriceInCurrency c = endpoint ++ "currentprice/" ++ c ++ ".json"
+
+-- | Returns history for month in USD
+-- 
+getHistory = endpoint ++ "historical/close.json"
+
+-- | Returns history for month in desired currency
+-- 
+getHistoryInCurrency c = endpoint ++ "historical/close.json?currency="++c
+
+getHistoryYesterday = getHistory ++ "?for=yesterday"
+
+getHistoryInPeriod :: Day -> Day -> String
+getHistoryInPeriod sdate edate = getHistory ++ "?start="++"&end="
+
+-- | Execute defined query over CoindDesk API
+-- 
+queryCD :: IO ()
+queryCD = putStrLn "someFunc"
